14693201ea638d902761f7e3c21901ed4e66ffc0
이찬우 <ichan-u@ichan-uui-MacBookAir.local>
2024-11-20 15:59:55 +0900

    7주차

---
+++ chan0831/spring/src/main/java/umc/study/service/tempService/TempCommandServiceImpl.java
@@ -0,0 +1,13 @@
+package umc.study.service.tempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandServiceImpl implements TempCommandService{
+    @Override
+    public void CheckFlag(Integer flag) {
+
+    }
+}
